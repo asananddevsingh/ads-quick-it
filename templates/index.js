a0a21f342bf609b64a2a9a6faa6637b70f87204a
asananddevsingh <ananddevsingh.aiet@gmail.com>
2022-05-04 02:33:34 +0530

    Added saga template export to index

--- templates/index.js
+++ templates/index.js
@@ -1,3 +1,4 @@
 export { default as React } from "./react";
 export { default as ReactApollo } from "./react-apollo";
 export { default as ReactReduxThunk } from "./react-redux-thunk";
+export { default as ReactReduxSaga } from "./react-redux-saga";
